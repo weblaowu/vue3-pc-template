7f01bce3f1ebce640060d7679306c1f2d74795c8
wuxie <stephen419@126.com>
2023-10-30 11:04:48 +0800

    perf: add nprogress

--- components.d.ts
+++ components.d.ts
@@ -8,13 +8,13 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     EditorWrap: typeof import('./src/components/EditorWrap/index.vue')['default']
-    FormWrap: typeof import('./src/components/global/FormWrap/index.vue')['default']
+    FormWrap: typeof import('./src/components/Global/FormWrap/index.vue')['default']
     Hello: typeof import('./src/components/Hello.vue')['default']
     MenuWrap: typeof import('./src/components/MenuWrap/index.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    SearchWrap: typeof import('./src/components/global/SearchWrap/index.vue')['default']
-    SecondWrap: typeof import('./src/components/global/SecondWrap/index.vue')['default']
+    SearchWrap: typeof import('./src/components/Global/SearchWrap/index.vue')['default']
+    SecondWrap: typeof import('./src/components/Global/SecondWrap/index.vue')['default']
     TButton: typeof import('tdesign-vue-next')['Button']
     TConfigProvider: typeof import('tdesign-vue-next')['ConfigProvider']
     TForm: typeof import('tdesign-vue-next')['Form']
--- src/permission.js
+++ src/permission.js
@@ -1,6 +1,17 @@
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 import router from './router'
+
+NProgress.configure({
+	showSpinner: false, // 是否显示加载ico
+})
 
 // 路由进入之前
 router.beforeEach((to, _, next) => {
+	NProgress.start()
 	next()
+})
+
+router.afterEach(() => {
+	NProgress.done()
 })
